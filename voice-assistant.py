64f56baf5640d7b8a829c28be8ccd15bfcf14eb3
Peter Moroz <ptr.moroz@gmail.com>
2021-11-23 21:17:35 +0300

    reworked a little

--- voice-assistant.py
+++ voice-assistant.py
@@ -6,41 +6,63 @@ import wikipedia
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
+
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+    
+def greet_me():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 6:
+        talk('Good night master !')
+    elif hour >= 6 and hour < 12:
+        talk('Good morning master !')
+    elif hour >= 12 and hour < 18:
+        talk('Good afternoon master !')
+    else:
+        talk('Good evening master !')
 
 def take_command():
     try:
         with sr.Microphone(device_index=0) as mic:
             recognizer.adjust_for_ambient_noise(mic)
-            print("say something")
+            print('listening ...')
             voice = recognizer.listen(mic)
-            instruct = recognizer.recognize_google(voice)
-            instruct = instruct.lower()
-            print(instruct)
-            return instruct
-    except Exception as ex:
-        print("Exception: " + str(ex))
+            print('recognizing ...')
+            text = recognizer.recognize_google(voice)
+            text = text.lower()
+            print(f"user said: {text}\n")
+    except sr.UnknownValueError:
+        print('Google speech recognition could not understand audio')
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; " + str(e))
+    except Exception as e:
+        print("Exception: " + str(e))
         return None
+    return text
         
 def run_assistant():
-    instruct = take_command()    
-    if 'time' in instruct:
-        time = datetime.datetime.now().strftime('%I: %M')
-        print(time)
+    cmd = take_command()    
+    if 'time' in cmd:
+        # time = datetime.datetime.now().strftime('%I: %M')
+        time = datetime.datetime.now().strftime('%H: %M: %S')
+        print(f"current time is: {time}\n")
         talk('current time is ' + time)
-    elif 'tell me about' in instruct:
-        thing = instruct.replace('tell me about', '')
-        info = wikipedia.summary(thing, 2)
+    elif 'tell me about' in cmd:
+        thing = cmd.replace('tell me about', '')
+        info = wikipedia.summary(thing, 3)
+        print(f"The Wikipedia tells about {thing}:\n")
         print(info)
         talk(info)
-    elif 'who are you' in instruct:
+    elif 'who are you' in cmd:
         talk('I am your personal assistant')
-    elif 'what can you do for me' in instruct:
+    elif 'what can you do for me' in cmd:
         talk('I can tell time and help you go with wikipedia')
     else:
-        talk('I did no understand, please repeat again')
+        talk('I did not understand, please repeat again')
 
-while True:
-    run_assistant()
+if __name__ == '__main__':
+    greet_me()
+    while True:
+        run_assistant()
