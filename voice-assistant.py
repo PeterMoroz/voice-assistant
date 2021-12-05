27ebfd168cd31b945c2f684e62a1f8f421712a3c
Peter Moroz <ptr.moroz@gmail.com>
2021-12-05 18:39:10 +0300

    add: resolve queries, find answers to questions with WolframAlpha

--- voice-assistant.py
+++ voice-assistant.py
@@ -3,6 +3,9 @@ import pyttsx3
 import requests
 import datetime
 import wikipedia
+import urllib.parse
+
+from pprint import pprint
 
 from configparser import ConfigParser
 
@@ -68,6 +71,101 @@ def get_the_weather_sentence(r):
     else:
        text = 'The weather service is unavailable. Repeat later please.'        
     return text
+    
+def wiki_search(q=''):
+    speak('Searching the wikipedia, please wait.')
+    info = wikipedia.summary(q)
+    print(f"The Wikipedia tells about {q}:\n{info}")
+    speak(info)
+
+def wolfram_alpha_query(q=''):
+    app_id = config.get('wolframalpha', 'app_id')
+    query = urllib.parse.quote_plus(q)
+    query_url = f"http://api.wolframalpha.com/v2/query?" \
+                f"appid={app_id}" \
+                f"&input={query}" \
+                f"&format=plaintext" \
+                f"&output=json"
+
+    response = requests.get(query_url)
+    if response == None:
+        print(f"No response to '{q}'\n")
+        # speak('No response from knowledges base')
+        return
+    
+    r = response.json().get('queryresult', {})
+    print('response:\n' + str(r))
+    if r['error']:
+    	print('Error in response')
+    	return
+
+    if not r['success']:
+    	print('Could not get answer')
+    	return
+
+    pod0 = r['pods'][0]
+    pod1 = r['pods'][1]
+    title = pod1.get('title', '').lower()
+    if ('definition' in title or 'result' in title or (pod1.get('primary', False))):
+        subpod = pod1['subpods'][0]
+        if isinstance(subpod, list):
+            answer = subpod[0].get('plaintext', '')
+        else:
+            answer = subpod.get('plaintext', '')
+        print(f"answer to '{q} is:\n{answer}")
+
+    else:
+        subpod = pod0['subpods'][0]
+        if isinstance(subpod, list):
+            question = subpod[0].get('plaintext', '')
+        else:
+            question = subpod.get('plaintext', '')
+        print(f"ask wikipedia about: '{question}'")
+        q = question.split('(')[0]
+        wiki_search(q)
+        
+def wolfram_alpha_solve(q=''):
+    app_id = config.get('wolframalpha', 'app_id')
+    query = urllib.parse.quote_plus(q)
+    query = urllib.parse.quote_plus(q)
+    query_url = f"http://api.wolframalpha.com/v2/query?" \
+                f"appid={app_id}" \
+                f"&input={query}" \
+                f"&includepodid=Result" \
+                f"&output=json"
+                
+    response = requests.get(query_url)
+    if response == None:
+        print(f"No response to '{q}'\n")
+        # speak('No response from solver')
+        return
+    
+    r = response.json().get('queryresult', {})
+    print('response:\n' + str(r))
+    if r['success'] and not r['error']:
+        pod0 = r['pods'][0]
+        if ((pod0.get('title', '').lower() == 'result') and (pod0.get('numsubpods', 0) > 0) and not (pod0.get('error', False))):
+            subpods = pod0['subpods'] 
+            answer = subpods[0].get('plaintext', '')
+            print(f"answer to '{q}' is {answer}\n")
+        else:
+            print(f"Could not solve '{q}'\n")
+            # speak('Could not solve.')
+    
+def wolfram_alpha_ask(q=''):
+    app_id = config.get('wolframalpha', 'app_id')
+    query_url = f"http://api.wolframalpha.com/v1/spoken?appid={app_id}&i={q}"
+    response = requests.get(query_url)
+    if response != None:
+        text = response.text
+        print(f"answer to '{q}' is:\n{text}")
+        speak(text)
+    else:
+        print('No response')
+        speak('No response')
+        
+# def wolfram_alpha_conversation():
+    
         
 def run_assistant():
     cmd = listen()    
@@ -80,20 +178,6 @@ def run_assistant():
         time = datetime.datetime.now().strftime('%H: %M: %S')
         print(f"current time is: {time}\n")
         speak('current time is ' + time)
-    elif 'tell me about' in cmd:
-        speak('Searching the wikipedia, please wait.')
-        thing = cmd.replace('tell me about', '')
-        info = wikipedia.summary(thing, 3)
-        print(f"The Wikipedia tells about {thing}:\n")
-        print(info)
-        speak(info)
-    elif 'search' in cmd:
-        speak('Searching the wikipedia, please wait.')
-        thing = cmd.replace('search', '')
-        info = wikipedia.summary(thing, 3)
-        print(f"The Wikipedia tells about {thing}:\n")
-        print(info)
-        speak(info)
     elif 'who are you' in cmd:
         speak('I am your personal assistant')
     elif 'weather' in cmd:
@@ -122,6 +206,22 @@ def run_assistant():
             speak(text)
         else:
             print('Could not get the weather sentence')
+            speak('Could not get the weather, try later please.')
+            
+    elif cmd.startswith('tell me about'):
+        # when command begins from 'tell me about' query the WolframAlpha and fallback to Wikipedia if failed
+        thing = cmd.replace('tell me about', '')    
+        wolfram_alpha_query(thing)
+    elif cmd.startswith('search'):
+        # when command begins from 'search' ask the Wikipedia
+        thing = cmd.replace('search', '')
+        wiki_search(thing)
+    elif cmd.startswith('solve'):
+        wolfram_alpha_solve(cmd)
+    elif cmd.startswith('who') or cmd.startswith('what') or cmd.startswith('where') or cmd.startswith('when') or cmd.startswith('why') or cmd.startswith('which') or cmd.startswith('how'):
+        wolfram_alpha_ask(cmd)
+    elif 'start conversation' in cmd or 'begin conversation' in cmd:
+        wolfram_alpha_conversation()
     else:
         speak("I'm sorry, I'm not sure I follow you.")
 
